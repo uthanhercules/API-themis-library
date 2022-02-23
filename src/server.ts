e8803d824fa8f7995e8035b3adfc0dce34ecd1e6
Uthan H <uthan.hercules12@gmail.com>
2022-02-23 10:58:37 -0300

    CORS implementation

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const routes = require('./router');
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
