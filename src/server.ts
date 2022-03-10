bd86a8c1052de40342f9f8b4cd01082f58446a85
Uthan H <uthan.hercules12@gmail.com>
2022-03-10 15:27:28 -0300

    Fixed bug on CORS

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+const cors = require('cors');
 const routes = require('./router');
 
 app.use(cors());
