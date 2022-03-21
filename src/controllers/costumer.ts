a155729b0dafadc76b9bdb235702c97f3cfc69de
dudafurtado <furtadoduda@hotmail.com>
2022-03-21 15:06:47 -0300

    First version of update customer function

--- src/controllers/costumer.ts
+++ src/controllers/costumer.ts
@@ -1,22 +1,19 @@
 import toast from '../messages/toasts';
 import knex from '../models/connection';
 
-const deleteCustomer = async (req: any, res: any) => {
+const updateCustomer = async (req: any, res: any) => {
     const { admin_id, full_name, email } = req.body;
 
     try {
-        const deleteCustomer = await knex('customers').delete({
-            id,
-            admin_id,
-            full_name,
-            query_code,
-            email,
-        });
+        const objetoParaAtualizar = {
+            email
+        }
+        await knex('customers').update(objetoParaAtualizar).where({ id });
 
-        return res.status(203).json('Cliente deletado com sucesso!');
+        return res.status(203).json('Cliente atualizado com sucesso!');
     } catch (error: any) {
         return res.status(400).json(toast.catchToast(error.message));
     }
 };
 
-export = { deleteCustomer }
+export = { updateCustomer }
--- src/router.ts
+++ src/router.ts
@@ -15,6 +15,6 @@ route.get('/admin/auth-verify', admin.authVerifyController);
 route.get('/procedure/list-recent', procedure.listLastFiveProcedures);
 route.post('/admin/new-user', admin.newUserController);
 
-route.delete('/customer/delete-customer', customer.deleteCustomer)
+route.put('/customer/update-customer', customer.updateCustomer)
 
 export = route;
