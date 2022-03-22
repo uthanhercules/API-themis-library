106f5fe49f35293ebb9418076553e5f053d54adc
dudafurtado <furtadoduda@hotmail.com>
2022-03-22 12:07:56 -0300

    Probably deleting the right way now :)

--- src/controllers/costumer.ts
+++ src/controllers/costumer.ts
@@ -1,17 +1,18 @@
 import toast from '../messages/toasts';
 import knex from '../models/connection';
 
+const { deleteCustomerSchema } = require('../validations/customerSchema');
+
 const deleteCustomer = async (req: any, res: any) => {
-    const { admin_id, full_name, email } = req.body;
+    const { id } = req.body;
 
     try {
-        const deleteCustomer = await knex('customers').delete({
-            id,
-            admin_id,
-            full_name,
-            query_code,
-            email,
-        });
+        const customerListById = await knex('customers').select('id').where({ id });
+        if (customerListById.length = 0) {
+            return res.status(400).json(toast.clientToast.error(5));
+        }
+
+        await knex('customers').delete().where({ id });
 
         return res.status(203).json('Cliente deletado com sucesso!');
     } catch (error: any) {
