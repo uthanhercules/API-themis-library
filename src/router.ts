e6b6b257aac1055c68bbe23b8d8bf8fd2c3ad405
Uthan H <uthan.hercules12@gmail.com>
2022-04-07 18:03:30 -0300

    New routes

--- src/router.ts
+++ src/router.ts
@@ -8,10 +8,10 @@ import loginVerify from './middlewares/loginVerify';
 const route = express();
 
 route.post('/login', admin.loginController);
-route.post('/admin/new-password', admin.newPasswordController);
+route.post('/new-password', admin.newPasswordController);
 
 route.use(loginVerify);
-route.get('/admin/auth-verify', admin.authVerifyController);
+route.get('/auth-verify', admin.authVerifyController);
 route.get('/procedure/list-recent', procedure.listLastFiveProcedures);
 
 route.post('/customer/create', customer.createCustomer);
