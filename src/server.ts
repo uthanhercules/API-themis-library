a141e02177b6f559ea8d6f9e3c0bd14a326702a8
Uthan H <uthan.hercules12@gmail.com>
2022-07-02 17:37:24 -0300

    Added heroku port

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,10 @@ dotenv.config();
 const app = express();
 const cors = require('cors');
 const routes = require('./router');
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(8000);
+app.listen(PORT);
